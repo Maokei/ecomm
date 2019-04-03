cb6d697023d0d8c61c91d08d0d4cda78b8212a16
maokei <rickard.joh@gmail.com>
2019-04-03 23:04:16 +0200

    Security configuration and utility

--- src/main/java/se/maokei/ecomm/store/config/SecurityConfig.java
+++ src/main/java/se/maokei/ecomm/store/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package se.maokei.ecomm.store.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import se.maokei.ecomm.store.services.UserSecurityService;
 
 @Configuration
 @EnableWebSecurity
@@ -15,16 +17,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserSecurityService userSecurityService;
+    private static final String[] PUBLIC_MATCHES = {
+            "/css/**", "/js/**","/image/**","/product/**","/book/**", "/user/**"
+
+    };
+
+    public SecurityConfig() {
+
+    }
 
     private BCryptPasswordEncoder passwordEncoder() {
         return SecurityUtility.passwordEncoder();
     }
 
-    private static final String[] PUBLIC_MATCHES = {
-
-    }
-
-    public SecurityConfig() {
-
-    }
+     @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable().httpBasic().and().authorizeRequests()
+                .antMatchers(PUBLIC_MATCHES).permitAll().anyRequest().authenticated();
+     }
 }
--- src/main/java/se/maokei/ecomm/store/config/SecurityUtility.java
+++ src/main/java/se/maokei/ecomm/store/config/SecurityUtility.java
@@ -1,4 +1,31 @@
 package se.maokei.ecomm.store.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+@Component
 public class SecurityUtility {
+    //TODO move to application.properties
+    private static final String SALT = "salt";
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
+
+    @Bean
+    public static String randomPassword() {
+        String characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXY1234567890#@$+";
+        StringBuilder newPassword = new StringBuilder();
+        Random rand = new Random();
+        while(newPassword.length() < 18) {
+            int index = (int) (rand.nextFloat() * characterSet.length());
+            newPassword.append(characterSet.charAt(index));
+        }
+        return newPassword.toString();
+    }
 }
