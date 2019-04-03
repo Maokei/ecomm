7900c0aabfc4bebf52069f6549cf39618ada5722
maokei <rickard.joh@gmail.com>
2019-04-03 23:12:46 +0200

    Role, UserRepository, SecurityService

--- src/main/java/se/maokei/ecomm/store/repository/UserRepository.java
+++ src/main/java/se/maokei/ecomm/store/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package se.maokei.ecomm.store.repository;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import se.maokei.ecomm.store.domain.User;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findUserByEmail(String email);
+    User findUserByUsername(String username);
+    List<User> findAll();
 }
--- src/main/java/se/maokei/ecomm/store/services/UserSecurityService.java
+++ src/main/java/se/maokei/ecomm/store/services/UserSecurityService.java
@@ -1,4 +1,28 @@
 package se.maokei.ecomm.store.services;
 
-public class UserSecurityService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import se.maokei.ecomm.store.domain.User;
+import se.maokei.ecomm.store.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserSecurityService.class);
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+        if(user == null) {
+            LOGGER.warn("Username not found {}", user);
+            throw new UsernameNotFoundException("Username " + username + "not found");
+        }
+        return user;
+    }
 }
