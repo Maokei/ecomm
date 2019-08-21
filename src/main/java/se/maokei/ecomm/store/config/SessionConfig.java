43112f2c9fb786ce4771cca0914d6766d2fb5e9d
maokei <rickard.joh@gmail.com>
2019-08-22 00:23:14 +0200

    Redis client lettuce, spring session redis dependencies

--- src/main/java/se/maokei/ecomm/store/config/SessionConfig.java
+++ src/main/java/se/maokei/ecomm/store/config/SessionConfig.java
@@ -1,8 +1,9 @@
 package se.maokei.ecomm.store.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
-public class HttpSessionConfig {
+public class SessionConfig {
 }
