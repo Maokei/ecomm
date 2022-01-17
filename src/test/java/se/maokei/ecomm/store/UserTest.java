c9c6adf60ca148403b47b166f60b08ed69fdd9d7
maokei <rickard.joh@gmail.com>
2022-01-17 14:10:53 +0100

    Updated test classes

--- src/test/java/se/maokei/ecomm/store/UserTest.java
+++ src/test/java/se/maokei/ecomm/store/UserTest.java
@@ -1,19 +1,16 @@
 package se.maokei.ecomm.store;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import se.maokei.ecomm.store.domain.User;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserTest {
     private User user;
 
-    @Before
-    private void setup() {
+    public void setup() {
         user = new User();
 
     }
